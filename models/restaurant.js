1dde76ff1db15a2673dfa24c6078d0683f54f9f3
olimarie21 <olivia.underdah@gmail.com>
2022-04-06 13:14:00 -0700

    update mongoose restaurant schema

--- models/restaurant.js
+++ models/restaurant.js
@@ -4,13 +4,24 @@ const Schema = mongoose.Schema;
 
 // Configure restaurant schema
 const RestaurantSchema = new Schema({
-    restaurant:{type:String, required:true},
-    website:{type:String, required:true},
-    address:{type:String, required:true},
+    restaurant:{
+        type:String, 
+        required: [true, 'Please enter the name of the restaurant.'],
+        minLength: [1, 'The restaurant\'\s name must be at least 1 character.'],
+        maxLength: [70, 'The restaurant\'\s name should not be longer than 70 characters.'] 
+    },
+    website: {
+        type:String, 
+        required: [true, "Please enter the restaurant\'\s website."]
+    },
+    address:{ 
+        type:String, 
+        required: [true, 'Please enter the restaurants address so we can provide directions!']
+    },
     type:{
         type:String,
-        enum: ["Thai", "Chinese", "Japanese", "Mexican", "American", "Fast food", "Vietnamese", "Vegan", "Vegetarian", "Indian", "Breakfast", "Other"],
-        required:true
+        enum: ["Thai", "Chinese", "Brazilian", "Japanese", "Mexican", "Cuban", "Korean", "American", "Fast food", "Vietnamese", "Vegan", "Vegetarian", "Indian", "Breakfast", "Other"],
+        required: [true, 'Please select a category for the restaurant.']
     },
     dateAdded:{type:Date, default: Date.now},
     visited:{type:Boolean, required: true, default:false},
