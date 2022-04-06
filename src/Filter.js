416fd254c22ee733c69dc3ec9e4d046df7e93660
olimarie21 <olivia.underdah@gmail.com>
2022-04-06 13:12:09 -0700

    filter restaurants

--- src/Filter.js
+++ src/Filter.js
@@ -1,9 +1,11 @@
-import React, {useState} from 'react';
+import axios from 'axios';
+import React, {useState, useRef} from 'react';
 import "./styles/filter.scss";
 
 export default function Filter(props) {
     const [chosenCategory, setCategory] = useState('');
     const [happyHour, setHappyHour] = useState(false);
+    const filterRef = props.getRef;
 
     const handleCategorySelect = (category) => {
         setCategory(category);
@@ -16,24 +18,29 @@ export default function Filter(props) {
     }
 
     return (
-        <div className='filterPopUp'>
-            <fieldset>
-                <legend>Food Category:</legend>
-                    {props.categories.map(category => (
-                        <div className='category' key={category}>
-                            <label>
-                                <input type='radio' name='category' value={category} onChange={()=>handleCategorySelect(category)}></input>
-                                {category}
-                            </label>
-                        </div>
-                    ))}
-            </fieldset>
-            <fieldset>
-                <legend>Happy hour?</legend>
-                <input type='checkbox' name='happyHour' checked={happyHour} onChange={handleHappyHour}></input>
-            </fieldset>
-            <button>apply</button>
-            <button>clear</button>
+        <div>
+            <div className='background'></div>
+            <div ref={filterRef} className='filterPopUp'>
+                <fieldset>
+                    <legend>Food Category:</legend>
+                        {props.categories.map(category => (
+                            <div className='category' key={category}>
+                                <label>
+                                    <input type='radio' name='category' value={category} onChange={()=>handleCategorySelect(category)}></input>
+                                    {category}
+                                </label>
+                            </div>
+                        ))}
+                </fieldset>
+                <fieldset>
+                    <legend>Happy hour?</legend>
+                    <input type='checkbox' name='happyHour' checked={happyHour} onChange={handleHappyHour}></input>
+                </fieldset>
+                <div className='filterButtons'>
+                    <button onClick={(e) => props.filterRestaurants(chosenCategory, happyHour)}>apply</button>
+                    <button onClick={e => props.filterRestaurants("", false)}>clear</button>
+                </div>
+            </div>
         </div>
     )
 };
